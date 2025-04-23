835a3597962c3916f2fb6e5ad8fb4e916d8fe5bd
HUGO\hugze <hugzeidan@outlook.com>
2025-04-23 16:30:13 +0200

    Some props spawn in doors

--- Assets/Scripts/PCG/Edgar/PropsProcess.cs
+++ Assets/Scripts/PCG/Edgar/PropsProcess.cs
@@ -10,11 +10,14 @@ using UnityEngine.Tilemaps;
 public class PropsProcess : DungeonGeneratorPostProcessBase
 {
     [SerializeField] private List<GameObject> props;
+    [SerializeField] private LayerMask doorsLayer;
+    private DoorManager _doorManager;
     Transform _parent;
-    
+
     private Tilemap _groundTilemap;
     private Tilemap _wallsTilemap;
-    
+
+
     private Vector3Int[] _Neighbours = new[]
     {
         new Vector3Int(0, 1, 0),
@@ -22,38 +25,40 @@ public class PropsProcess : DungeonGeneratorPostProcessBase
         new Vector3Int(0, -1, 0),
         new Vector3Int(-1, 0, 0),
     };
-    
+
     public override void Run(GeneratedLevel level, LevelDescription levelDescription)
     {
+        _doorManager = level.RootGameObject.GetComponent<DoorManager>();
+        
         _groundTilemap = level.GetSharedTilemaps()[0];
         _wallsTilemap = level.GetSharedTilemaps()[1];
-        
+
         _parent = level.RootGameObject.transform.parent.Find("Props");
 
-        Debug.Log(_parent.name);
-
-        for (int i = _parent.childCount-1; i >= 0; i--)
+        for (int i = _parent.childCount - 1; i >= 0; i--)
         {
             DestroyImmediate(_parent.GetChild(i).gameObject);
         }
-        
+
         for (int i = _groundTilemap.origin.x; i < _groundTilemap.origin.x + _groundTilemap.size.x; i++)
         {
             for (int j = _groundTilemap.origin.y; j < _groundTilemap.origin.y + _groundTilemap.size.y; j++)
             {
-                if (_groundTilemap.HasTile(new Vector3Int(i,j)))
+                if (_groundTilemap.HasTile(new Vector3Int(i, j)))
                 {
-                    foreach (var neighbour in _Neighbours)
+                    if (!_doorManager.HasDoor(new Vector2(i + 0.5f, j + 0.5f)))
                     {
-                        if (_wallsTilemap.HasTile(new Vector3Int(i,j) + neighbour))
+                        foreach (var neighbour in _Neighbours)
                         {
-                            
-                            int rnd = UnityEngine.Random.Range(0, 100) % 3;
-                            if (rnd == 0)
+                            if (_wallsTilemap.HasTile(new Vector3Int(i, j) + neighbour))
                             {
-                                Instantiate(props[UnityEngine.Random.Range(0, props.Count)], new Vector3(i + 0.5f, j + 0.5f), Quaternion.identity, _parent);
+                                int rnd = UnityEngine.Random.Range(0, 100) % 3;
+                                if (rnd == 0)
+                                {
+                                    Instantiate(props[UnityEngine.Random.Range(0, props.Count)], new Vector3(i, j), Quaternion.identity, _parent);
+                                }
+                                break;
                             }
-                            break;
                         }
                     }
                 }
